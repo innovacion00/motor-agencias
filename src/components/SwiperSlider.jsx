e8df258c423725dda7133df711246af542439285
SebasKB <sekelbi99@hotmail.com>
2024-09-25 09:52:53 -0500

    Updated

--- src/components/SwiperSlider.jsx
+++ src/components/SwiperSlider.jsx
@@ -9,6 +9,7 @@ import 'swiper/css/pagination';
 
 
 
+
 const SwiperSlider = () => {
   return (
     <Swiper
@@ -17,18 +18,16 @@ const SwiperSlider = () => {
       slidesPerView= {1}
       navigation
       pagination={{ clickable: true }}
-      autoplay={{ delay: 3000, disableOnInteraction: true }} // Configuración de autoplay
+      autoplay={{ delay: 2000, disableOnInteraction: true }} // Configuración de autoplay
       loop={true} // Habilitar bucle
       
     >
+      
       <SwiperSlide>
-        <img src="/public/images/slide.png" alt="Imagen aixo" />
+        <img src="/public/images/slide1.jpeg" alt="Banner 1" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/public/images/slide1.png" alt="Imagen bocagrande" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="/public/images/slide2.png" alt="Imagen avexi" />
+        <img src="/public/images/slide2.jpeg" alt="Banner 2" />
       </SwiperSlide>
     </Swiper>
   );
