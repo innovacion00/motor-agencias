5b59e9a7a006408ccf682dfc74a9abc159f36a60
SebasKB <sekelbi99@hotmail.com>
2024-10-09 08:53:23 -0500

    Updated

---
+++ src/stores/searchStores.js
@@ -0,0 +1,21 @@
+import { atom } from 'nanostores';
+
+// Definimos los estados globales para la búsqueda
+export const searchStore = atom({
+  dateRange: {
+    startDate: new Date(),
+    endDate: new Date(),
+  },
+  adults: 1,
+  children: 0,
+  rooms: 1,
+  childrenAges: [],
+});
+
+// Funciones para actualizar la store global
+export const updateSearchStore = (newState) => {
+  searchStore.set({
+    ...searchStore.get(),
+    ...newState,
+  });
+};
