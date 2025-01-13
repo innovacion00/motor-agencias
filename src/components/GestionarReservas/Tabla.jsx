accd9ac825615ee581841d0d0249cc44d7620e60
SebasKB <sekelbi99@hotmail.com>
2025-01-13 16:36:36 -0500

    Barra de busqueda ( por hotel codigo o nombre de huesped)

--- src/components/GestionarReservas/Tabla.jsx
+++ src/components/GestionarReservas/Tabla.jsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles/tabla.module.css";
 import { getReservas, reservasNano } from "../../stores/disponibilidad";
+
 const Tabla = () => {
   const [reservas, setreservas] = useState([]);
   const [tokenUrl, setTokenUrl] = useState("");
   const [nombreAgencia, setnombreAgencia] = useState("");
-// console.log(role)
+  const [searchTerm, setSearchTerm] = useState(""); // Nuevo estado para el filtro
+  const [filteredReservas, setFilteredReservas] = useState([]); // Estado para las reservas filtradas
+
+  // console.log(role)
   useEffect(() => {
     const datosUsuario = JSON.parse(localStorage.getItem("datosUsuario"));
-    // console.log(datosUsuario) // Datos del usuario y reserva
     ObtenerReservas(datosUsuario.token, datosUsuario.role[0]);
     setTokenUrl(datosUsuario.token);
     setnombreAgencia(datosUsuario);
   }, []);
+
   const ObtenerReservas = async (token, nombreAgencia) => {
-
     await getReservas(token, nombreAgencia);
-    setreservas(reservasNano.get());
+    const reservasObtenidas = reservasNano.get();
+    setreservas(reservasObtenidas);
+    setFilteredReservas(reservasObtenidas); // Inicializar reservas filtradas
   };
-
   console.log(reservas);
-
   //funcion para formatear el los valores de dinero
   const formatCurrency = (value) => {
     if (value === undefined || value === null || isNaN(value)) {
@@ -32,96 +35,113 @@ const Tabla = () => {
     }).format(value);
   };
 
-  // const reservas = reservasNano.get()
+  const handleSearch = (event) => {
+    const searchValue = event.target.value.toLowerCase();
+    setSearchTerm(searchValue);
+
+    const filtered = reservas.filter((reserva) =>
+      reserva.hotel.toLowerCase().includes(searchValue) || // Filtrar por hotel
+      reserva.reservaChatbotId.toString().includes(searchValue) || // Filtrar por código de reserva
+      reserva.reservation.firstName.toLowerCase().includes(searchValue) || // Filtrar por nombre del huésped
+      reserva.reservation.lastName.toLowerCase().includes(searchValue) // Filtrar por apellido del huésped
+    );
+    setFilteredReservas(filtered);
+  };
+
   return (
-    <>
-    
-      <div className={styles.container}>
-        <br />
-        <h1>Consultar mis reservas</h1>
-        <div className={styles.filters}>
-          <button>
-            <i className="fas fa-filter"></i> Filtros
-          </button>
-        </div>
-        <div className={styles.tabs}>
-          <div className={styles.active}>
-            Próximos pagos {/*<span className={styles.badge}>1</span>*/}
-          </div>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Cod. reserva</th>
-              <th>Hotel</th>
-              <th>Check-in</th>
-              <th>Check-out</th>
-              <th>Agencia</th>
-              <th>Huesped</th>
-              <th>Plazo para pagar</th>
-              <th>Valor a pagar</th>
-              <th>Estado</th>
-              <th>Acciones</th>
-              
-            </tr>
-          </thead>
-          <tbody>
-            {reservas.map((dato, index) => (
-              <tr key={index}>
-                <td>{dato.reservaChatbotId}</td>
-                <td>{dato.hotel}</td>
-                <td>{dato.reservation.checkin}</td>
-                <td>{dato.reservation.checkout}</td>
-                <td>{dato?.agenciaId?.fullName}</td>
-                <td>{`${dato.reservation.firstName} ${dato.reservation.lastName}`}</td>
-                <td>{dato.fechaLimitePago}</td>
-                <td>{formatCurrency(dato.total)}</td>
-                <td>
-                  {dato.status == "0" ? (
-                    <span className={`${styles.status} ${styles.pending}`}>
-                      Pago pendiente
-                    </span>
-                  ) : dato.status == "1" ? (
-                    <span className={`${styles.status} ${styles.proces}`}>
-                      Pago en proceso
-                    </span>
-                  ) : dato.status == "2" ? (
-                    <span className={`${styles.status} ${styles.denied}`}>
-                      Pago rechazado
-                    </span>
-                  ) : dato.status == "3" ? (
-                    <span className={`${styles.status} ${styles.clomplete}`}>
-                      Pago aprobado
-                    </span>
-                  ) : dato.status == "4" ? (
-                    <span className={`${styles.status} ${styles.cancel}`}>
-                      Reserva cancelada
-                    </span>
-                  ) : (
-                    <p>Estado no valido</p>
-                  )}
-                </td>
-                <td>
-                  <a
-                    href={`/gestionar/${dato.reservaChatbotId}`}
-                    className={styles.link}
-                  >
-                    Consultar y gestionar
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className={styles.pagination}>
-          <span>Anterior</span>
-          <a href="#" className={styles.active}>
-            1
-          </a>
-          <span>Siguiente</span>
+    <div className={styles.container}>
+      <br />
+      <h1>Consultar mis reservas</h1>
+
+      {/* Filtro de búsqueda */}
+      <div className={styles.filters}>
+        <input
+          type="text"
+          placeholder="Buscar por hotel, huésped o código de reserva"
+          value={searchTerm}
+          onChange={handleSearch}
+          className={styles.searchInput}
+        />
+      </div>
+
+      <div className={styles.tabs}>
+        <div className={styles.active}>
+          Próximos pagos {/*<span className={styles.badge}>1</span>*/}
         </div>
       </div>
-    </>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Cod. reserva</th>
+            <th>Hotel</th>
+            <th>Check-in</th>
+            <th>Check-out</th>
+            <th>Agencia</th>
+            <th>Huésped</th>
+            <th>Plazo para pagar</th>
+            <th>Valor a pagar</th>
+            <th>Estado</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredReservas.map((dato, index) => (
+            <tr key={index}>
+              <td>{dato.reservaChatbotId}</td>
+              <td>{dato.hotel}</td>
+              <td>{dato.reservation.checkin}</td>
+              <td>{dato.reservation.checkout}</td>
+              <td>{dato?.agenciaId?.fullName}</td>
+              <td>{`${dato.reservation.firstName} ${dato.reservation.lastName}`}</td>
+              <td>{dato.fechaLimitePago}</td>
+              <td>{formatCurrency(dato.total)}</td>
+              <td>
+                {dato.status == "0" ? (
+                  <span className={`${styles.status} ${styles.pending}`}>
+                    Pago pendiente
+                  </span>
+                ) : dato.status == "1" ? (
+                  <span className={`${styles.status} ${styles.proces}`}>
+                    Pago en proceso
+                  </span>
+                ) : dato.status == "2" ? (
+                  <span className={`${styles.status} ${styles.denied}`}>
+                    Pago rechazado
+                  </span>
+                ) : dato.status == "3" ? (
+                  <span className={`${styles.status} ${styles.clomplete}`}>
+                    Pago aprobado
+                  </span>
+                ) : dato.status == "4" ? (
+                  <span className={`${styles.status} ${styles.cancel}`}>
+                    Reserva cancelada
+                  </span>
+                ) : (
+                  <p>Estado no válido</p>
+                )}
+              </td>
+              <td>
+                <a
+                  href={`/gestionar/${dato.reservaChatbotId}`}
+                  className={styles.link}
+                >
+                  Consultar y gestionar
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className={styles.pagination}>
+        <span>Anterior</span>
+        <a href="#" className={styles.active}>
+          1
+        </a>
+        <span>Siguiente</span>
+      </div>
+    </div>
   );
 };
 
