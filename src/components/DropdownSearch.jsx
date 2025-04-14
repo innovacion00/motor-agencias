1561893b43db3634be31cd181751b4da7a5409e6
SebasKB <sekelbi99@hotmail.com>
2025-04-14 09:16:57 -0500

    hotfix array de disponibilidad

--- src/components/DropdownSearch.jsx
+++ src/components/DropdownSearch.jsx
@@ -113,7 +113,7 @@ const DropdownSearch = () => {
   const handleSingleReservation = () => {
     setRooms(
       Array.from({ length: 1 }, () => ({
-        adults: 2,
+        adults: 1,
         children0to4: 0,
         children5to17: 0,
       }))
@@ -283,7 +283,7 @@ const DropdownSearch = () => {
                   <div className={styles.counter}>
                     <button
                       onClick={() => {
-                        if (room.adults > 2) {
+                        if (room.adults > 1) {
                           const updatedRooms = [...rooms];
                           updatedRooms[index].adults -= 1;
                           setRooms(updatedRooms);
