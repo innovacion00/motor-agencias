e3f040ddeea78802e09e3833eb9d7ef04c72f4d9
Innovacion <innovacion@gehsuites.com>
2025-08-22 12:49:21 -0500

    validacion de undefined para redirigir al login

--- src/stores/authtoken.js
+++ src/stores/authtoken.js
@@ -1,6 +1,12 @@
-
 import { atom } from "nanostores";
-
-
+import Cookies from "js-cookie";
 
 export const tokenglobal = atom("13");
+
+export const validarToken = async () => {
+  const token = Cookies.get("token");
+
+  if (!token || token == 'undefined') {
+    window.location.href = "/login";
+  }
+};
