0207a9a0fe21dccf2e259631eda880201854f457
SebasKB <sekelbi99@hotmail.com>
2025-09-26 08:19:08 -0500

    Creacion de la pestaña cotizaciones

--- src/components/Userdashboard.jsx
+++ src/components/Userdashboard.jsx
@@ -290,12 +290,13 @@ const UserDashboard = () => {
         {userData && userData?.role && userData?.role.includes("super-admin")&&(
         <a href="/estadisticas">Análisis de datos</a>
       )} 
+      <a href="Cotizaciones">Cotizaciones</a>
+      
+      <a href="/ultimosmovimientos" target="_blank" rel="noopener noreferrer">Ultimo movimientos</a>
         {/* Mostrar el enlace de configuración solo si el usuario tiene rol de admin */}
         {userData && userData?.role && (userData?.role.includes("admin") || userData?.role.includes("super-admin")) && (
         <a href="/configuracion">Configuración</a>
       )}
-      
-      <a href="/ultimosmovimientos" target="_blank" rel="noopener noreferrer">Ultimo movimientos</a>
         <button onClick={handleLogout}>
           Cerrar sesión
         </button>
