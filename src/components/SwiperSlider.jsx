697671135a859dbe55b9f9a95583eef6a636798c
SebasKB <sekelbi99@hotmail.com>
2025-11-24 15:00:56 -0500

    De vuelta los sliders

--- src/components/SwiperSlider.jsx
+++ src/components/SwiperSlider.jsx
@@ -22,10 +22,10 @@ const SwiperSlider = () => {
       
     >
       {/* Banner servicio temporalmente inactivo */}
-      <SwiperSlide >
-        <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/ChatGPT%20Image%2024%20nov%202025,%2009_48_26%20a.m..png" alt="Banner playa salguero" />
-      </SwiperSlide>
       {/* <SwiperSlide >
+        <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/ChatGPT%20Image%2024%20nov%202025,%2009_48_26%20a.m..png" alt="Banner playa salguero" />
+      </SwiperSlide> */}
+      <SwiperSlide >
         <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/banner%20cotizador.jpg" alt="Banner cotizar 2" />
       </SwiperSlide>
       <SwiperSlide >
@@ -44,7 +44,7 @@ const SwiperSlider = () => {
       
       <SwiperSlide>
         <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/slide2.jpeg" alt="Banner principal" />
-      </SwiperSlide> */}
+      </SwiperSlide>
     </Swiper>
   );
 };
