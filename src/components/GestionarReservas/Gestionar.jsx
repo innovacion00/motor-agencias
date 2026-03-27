40ff8ba489af36de9d0c0480cf75e93ac7e4c764
carlosceballos1 <carlosdceballos30@gmail.com>
2026-03-27 10:43:35 -0500

    se agregó correo de angelica para el selector de cambios de estados reservas

--- src/components/GestionarReservas/Gestionar.jsx
+++ src/components/GestionarReservas/Gestionar.jsx
@@ -1561,7 +1561,7 @@ const Gestionar = ({ reservas }) => {
             <div className={styles.acciones}>
               {/* Cambiar estado de la reserva - visible para super-admin o correos autorizados */}
               {(datosDelUsuario?.role?.includes("super-admin") &&
-                ["carlosdceballos30@gmail.com", "innovacion@gehsuites.com", "malejadigital97@gmail.com","yltamara21@gmail.com"].includes(
+                ["carlosdceballos30@gmail.com", "innovacion@gehsuites.com", "malejadigital97@gmail.com","yltamara21@gmail.com","angelicavreservas@gmail.com"].includes(
               
                   datosDelUsuario?.email?.toLowerCase?.() ?? ""
                 )) && (
