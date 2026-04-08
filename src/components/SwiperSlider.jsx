61344cf639904a23a1d189217fb20a36190543e3
SebasKB <sekelbi99@hotmai.com>
2026-04-08 13:07:13 -0500

    banner junto con correccion de beds (TEMPORAL)

--- src/components/SwiperSlider.jsx
+++ src/components/SwiperSlider.jsx
@@ -22,14 +22,14 @@ const SwiperSlider = () => {
       
     >
       {/* Banner servicio temporalmente inactivo */}
-      <SwiperSlide >
+      {/* <SwiperSlide >
         <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/ChatGPT%20Image%2024%20nov%202025,%2009_48_26%20a.m..png" alt="Banner servicio temporalmente inactivo" />
-      </SwiperSlide>
+      </SwiperSlide> */}
       {/* <SwiperSlide>
       <a href="/BookingConnectIA">
         <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/banner%20geh%20ia.jpg" alt="Banner LucIA" />
         </a>
-      </SwiperSlide>
+      </SwiperSlide> */}
       <SwiperSlide >
         <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/Banner%20playa%20salguero.jpg" alt="Banner playa salguero" />
       </SwiperSlide>
@@ -49,7 +49,7 @@ const SwiperSlider = () => {
       
       <SwiperSlide>
         <img src="https://space-img.sfo3.digitaloceanspaces.com/Agencias/slide2.jpeg" alt="Banner principal" />
-      </SwiperSlide> */}
+      </SwiperSlide>
     </Swiper>
   );
 };
