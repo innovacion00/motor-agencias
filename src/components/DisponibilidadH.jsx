95a967c2d8d76342e261732edb2537a076da4411
SebasKB <sekelbi99@hotmai.com>
2026-06-01 11:33:30 -0500

    correccion en reges de precios en habitaciones

--- src/components/DisponibilidadH.jsx
+++ src/components/DisponibilidadH.jsx
@@ -1032,7 +1032,7 @@ export const Cid = ({ id }) => {
   };
 
   const regexSeleccionado =
-    categoriagencia == 696
+    categoriagencia == 0
       ? regexminoristas[planDeAlimentacion]
       : regexMayorista[planDeAlimentacion];
 
